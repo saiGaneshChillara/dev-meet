68fabb3e1d14411e62f150e53c78c908e1a7c3db
cvlnpsg987 <cvlnpsg987@gmail.com>
2025-10-29 11:55:28 +0000

    setup done

---
+++ demo-app/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import "./globals.css";
+import LightRays from "../components/LightRays";
+
+const schibstedGrostek = Schibsted_Grotesk({
+	variable: "--font-schibsted-grotesk",
+	subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+	variable: "--font-martian-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "DevEvent",
+	description: "The hub for every dev event that you mustn't miss",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={`${schibstedGrostek.variable} ${martianMono.variable} antialiased min-h-screen`}
+			>
+				<div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+					<LightRays
+						raysOrigin="top-center-offset"
+						raysColor="#5dfeca"
+						raysSpeed={0.5}
+						lightSpread={0.9}
+						rayLength={1.4}
+						followMouse={true}
+						mouseInfluence={0.02}
+						noiseAmount={0.0}
+						distortion={0.01}
+					/>
+				</div>
+				<main>
+					{children}
+				</main>
+			</body>
+		</html>
+	);
+}
---
+++ demo-app/app/page.tsx
@@ -0,0 +1,12 @@
+// import Hello from "../components/hello";
+
+const Home = () => {
+	return (
+		<section>
+			<h1 className="text-center">The Hub for Every Dev <br /> Event you can't miss</h1>
+			<p className="text-center mt-5">Hackthons, Meetups and conferences, All in one place</p>
+		</section>
+	);
+};
+
+export default Home;
